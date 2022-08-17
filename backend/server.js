3f6b3ffe7b209ef2dd3db1658c23bb5b7d7519a1
Jonathan <jonathancarpena99@gmail.com>
2022-08-17 16:30:50 -0700

    Fixing deployment bug

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use('/image', imageRoutes)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get("/", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
     })
 }
