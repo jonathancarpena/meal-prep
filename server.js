33f2f9efc5da4362e1ec94115911c2fd482ba4dc
Jonathan <jonathancarpena99@gmail.com>
2022-08-17 17:24:56 -0700

    Remove Port Env Var, Changed to path.resolve

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use('/image', imageRoutes)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"))
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
     })
 }
 
