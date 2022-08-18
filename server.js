c6f37f77fbc8930dffa9077d68d499fcb259e185
Jonathan <jonathancarpena99@gmail.com>
2022-08-17 17:40:15 -0700

    Changed proxy on frontend, Switched resolve to join

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ app.use("/api/orders", orderRoutes)
 app.use('/image', imageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"))
+    app.use(express.static(path.join(__dirname, "frontend", "build")))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
     })
 }
 
