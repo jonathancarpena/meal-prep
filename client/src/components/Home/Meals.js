84af66c3e0041f12430bdccf3270029b4bf0abd7
Jonathan Carpena <63978806+jonathancarpena@users.noreply.github.com>
2022-09-12 16:15:59 -0700

    Fixed landing page tablet responsive

--- client/src/components/Home/Meals.js
+++ client/src/components/Home/Meals.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,127 +9,125 @@ import 'swiper/css';
 import 'swiper/css/scrollbar';
 
 // Images
-import meal1 from '../../images/home/meals-sample-1.jpg'
-import meal2 from '../../images/home/meals-sample-2.jpg'
-import meal3 from '../../images/home/meals-sample-3.jpg'
+import meal1 from '../../images/home/meals-sample-1.jpg';
+import meal2 from '../../images/home/meals-sample-2.jpg';
+import meal3 from '../../images/home/meals-sample-3.jpg';
 
 // Icons
-import { BsInfoCircleFill } from 'react-icons/bs'
-import { MdClose } from 'react-icons/md'
-
-
+import { BsInfoCircleFill } from 'react-icons/bs';
+import { MdClose } from 'react-icons/md';
 
 function Card({ item }) {
-    const [showInfo, setShowInfo] = useState(false)
-    return (
-        <div className='w-full relative'>
-            <img alt={item.text} src={item.img} className='object-cover' />
-            <button onClick={() => setShowInfo(!showInfo)} className='bg-yellow-400 p-2 rounded-lg transition-all ease-in-out duration-200 absolute top-5 right-5 z-[50]  text-white hover:text-bg-yellow-500 hover:scale-110 active:scale-90 drop-shadow-md'>
-                <BsInfoCircleFill className='text-[1.5rem]' />
-            </button>
-            <div className={`${showInfo ? 'translate-y-0' : 'translate-y-full'} select-none flex flex-col justify-evenly transition-all ease-in-out duration-200 bg-white opacity-90 h-[40%] p-8 md:p-10 bottom-0 w-full absolute`}>
-                <h3 className='text-lg lg:text-2xl font-bold lg:mb-4'>{item.info.name}</h3>
-                <ul>
-                    <li className='lg:text-lg'>Calories: {item.info.cal} cals</li>
-                    <li className='lg:text-lg'>Protein: {item.info.protein}g</li>
-                    <li className='lg:text-lg'>Carbs: {item.info.carbs}g</li>
-                    <li className='lg:text-lg'>Fats: {item.info.fats}g</li>
-                </ul>
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <div className='w-full relative'>
+      <img alt={item.text} src={item.img} className='object-cover' />
+      <button
+        onClick={() => setShowInfo(!showInfo)}
+        className='bg-yellow-400 p-2 rounded-lg transition-all ease-in-out duration-200 absolute top-5 right-5 z-[50]  text-white hover:text-bg-yellow-500 hover:scale-110 active:scale-90 drop-shadow-md'>
+        <BsInfoCircleFill className='text-[1.5rem]' />
+      </button>
+      <div
+        className={`${
+          showInfo ? 'translate-y-0' : 'translate-y-full'
+        } select-none flex flex-col justify-evenly transition-all ease-in-out duration-200 bg-white opacity-90 h-[40%] p-8 md:p-10 bottom-0 w-full absolute`}>
+        <h3 className='text-lg lg:text-2xl font-bold lg:mb-4'>
+          {item.info.name}
+        </h3>
+        <ul>
+          <li className='lg:text-lg'>Calories: {item.info.cal} cals</li>
+          <li className='lg:text-lg'>Protein: {item.info.protein}g</li>
+          <li className='lg:text-lg'>Carbs: {item.info.carbs}g</li>
+          <li className='lg:text-lg'>Fats: {item.info.fats}g</li>
+        </ul>
 
-                <button onClick={() => setShowInfo(false)} className='active:scale-90 transition-all ease-in-out duration-200 bg-neutral-200 px-2 rounded-lg absolute top-4 right-4 md:top-6 md:right-6 flex space-x-1 items-center'>
-                    <MdClose />
-                    <span>Close</span>
-                </button>
-            </div>
-        </div>
-    )
+        <button
+          onClick={() => setShowInfo(false)}
+          className='active:scale-90 transition-all ease-in-out duration-200 bg-neutral-200 px-2 rounded-lg absolute top-4 right-4 md:top-6 md:right-6 flex space-x-1 items-center'>
+          <MdClose />
+          <span>Close</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 function Meals() {
-    const MealsContent = [
-        {
-            text: "Breakfast",
-            img: meal1,
-            info: {
-                name: 'Super French Toast',
-                cal: 256,
-                protein: 43,
-                carbs: 23,
-                fats: 10
-            }
+  const MealsContent = [
+    {
+      text: 'Breakfast',
+      img: meal1,
+      info: {
+        name: 'Super French Toast',
+        cal: 256,
+        protein: 43,
+        carbs: 23,
+        fats: 10,
+      },
+    },
+    {
+      text: 'Lunch',
+      img: meal2,
+      info: {
+        name: 'Anabolic Pesto Pasta',
+        cal: 256,
+        protein: 43,
+        carbs: 23,
+        fats: 10,
+      },
+    },
+    {
+      text: 'Dinner',
+      img: meal3,
+      info: {
+        name: 'Creamy Chicken w/ Spinach',
+        cal: 256,
+        protein: 43,
+        carbs: 23,
+        fats: 10,
+      },
+    },
+  ];
 
-        },
-        {
-            text: "Lunch",
-            img: meal2,
-            info: {
-                name: 'Anabolic Pesto Pasta',
-                cal: 256,
-                protein: 43,
-                carbs: 23,
-                fats: 10
-            }
+  return (
+    <div className='bg-neutral-100 px-10 py-24 z-[30] flex flex-col md:flex-row-reverse md:px-5 lg:px-20 justify-around items-center'>
+      {/* Header */}
+      <div className='flex flex-col justify-around mb-12 md:mb-0 md:w-[45%]'>
+        <h2 className=' text-5xl tracking-tight font-bold text-center uppercase lg:mb-10 lg:text-7xl '>
+          THE Meals<span className='text-yellow-500'>.</span>
+        </h2>
 
-        },
-        {
-            text: "Dinner",
-            img: meal3,
-            info: {
-                name: 'Creamy Chicken w/ Spinach',
-                cal: 256,
-                protein: 43,
-                carbs: 23,
-                fats: 10
-            }
-        },
-    ]
-
-
-    return (
-        <div className='bg-neutral-100 px-10 py-24 z-[30] flex flex-col md:flex-row-reverse lg:px-20 justify-around items-center'>
-
-            {/* Header */}
-            <div className='flex flex-col justify-around mb-12 md:mb-0 md:w-[45%]'>
-                <h2 className=' text-5xl tracking-tight font-bold text-center uppercase lg:mb-10 lg:text-7xl '>
-                    THE Meals<span className='text-yellow-500'>.</span>
-                </h2>
-
-                {/* Sub Header */}
-                <p className='text-center text-base lg:text-xl mt-5 text-neutral-700 '>
-                    {`Our dishes are tailored to meet individual dietary needs. 
+        {/* Sub Header */}
+        <p className='text-center text-base lg:text-xl mt-5 text-neutral-700 '>
+          {`Our dishes are tailored to meet individual dietary needs. 
                     Order á la carte or opt for a meal plan. We offer Chef’s Choice, 
                     pre-chosen meal plans or Your Choice, fully customizable meal plans.`}
-                </p>
-            </div>
+        </p>
+      </div>
 
-            {/* Content */}
-            <div className='w-[380px] md:w-[400px] lg:w-[500px]'>
-                <Swiper
-                    slidesPerView={1}
-                    modules={[Autoplay]}
-                    className=' rounded-xl overflow-hidden drop-shadow-xl cursor-grab active:cursor-grabbing'
-                    autoplay={{
-                        delay: 3,
-                        disableOnInteraction: true,
-                    }}
-                    freeMode={true}
-                    loop={true}
-                    speed={5000}
-
-                >
-                    {MealsContent.map((item) => (
-                        <SwiperSlide key={item.text} className=' w-full relative mb-5   flex items-center justify-center rounded-xl overflow-hidden'>
-                            <Card item={item} />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-
-
-
-
-
-        </div>
-    )
+      {/* Content */}
+      <div className='w-[380px] md:w-[400px] lg:w-[500px]'>
+        <Swiper
+          slidesPerView={1}
+          modules={[Autoplay]}
+          className=' rounded-xl overflow-hidden drop-shadow-xl cursor-grab active:cursor-grabbing'
+          autoplay={{
+            delay: 3,
+            disableOnInteraction: true,
+          }}
+          freeMode={true}
+          loop={true}
+          speed={5000}>
+          {MealsContent.map((item) => (
+            <SwiperSlide
+              key={item.text}
+              className=' w-full relative mb-5   flex items-center justify-center rounded-xl overflow-hidden'>
+              <Card item={item} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 
-export default Meals
+export default Meals;
--- client/src/components/Home/Reviews.js
+++ client/src/components/Home/Reviews.js
@@ -1,5 +1,4 @@
-import React, { useRef, useCallback } from 'react'
-
+import React, { useRef, useCallback } from 'react';
 
 // Import Swiper React components
 import { Pagination } from 'swiper';
@@ -11,104 +10,109 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // Icons
-import { ImQuotesRight } from 'react-icons/im'
-import { GoChevronLeft, GoChevronRight } from 'react-icons/go'
-
-
+import { ImQuotesRight } from 'react-icons/im';
+import { GoChevronLeft, GoChevronRight } from 'react-icons/go';
 
 function Reviews() {
-    const sliderRef = useRef(null);
+  const sliderRef = useRef(null);
 
-    const handlePrev = useCallback(() => {
-        if (!sliderRef.current) return;
-        sliderRef.current.swiper.slidePrev();
-    }, []);
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
 
-    const handleNext = useCallback(() => {
-        if (!sliderRef.current) return;
-        sliderRef.current.swiper.slideNext();
-    }, []);
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, []);
 
-    const ReviewsContent = [
-        {
-            author: 'John K.',
-            content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  const ReviewsContent = [
+    {
+      author: 'John K.',
+      content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Et nam veritatis soluta saepe odit voluptatibus. Eos non ratione 
             pariatur dolorum nihil quo. Inventore ratione sint accusamus impedit 
             amet aliquid vel.
-            `
-        },
-        {
-            author: 'Veronica G.',
-            content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            `,
+    },
+    {
+      author: 'Veronica G.',
+      content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Et nam veritatis soluta saepe odit voluptatibus. Eos non ratione 
             pariatur dolorum nihil quo. Inventore ratione sint accusamus impedit 
             amet aliquid vel.
-            `
-        },
-        {
-            author: 'Adam S.',
-            content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            `,
+    },
+    {
+      author: 'Adam S.',
+      content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Et nam veritatis soluta saepe odit voluptatibus. Eos non ratione 
             pariatur dolorum nihil quo. Inventore ratione sint accusamus impedit 
             amet aliquid vel.
-            `
-        },
-    ]
-    return (
-        <div className='py-32 bg-[#333333] rounded-2xl'>
+            `,
+    },
+  ];
+  return (
+    <div className='py-32 bg-[#333333] rounded-2xl'>
+      {/* Header */}
+      <h2 className=' text-white text-5xl tracking-tight font-bold text-center uppercase lg:mb-10 lg:text-7xl '>
+        See What People are saying<span className='text-yellow-500'>.</span>
+      </h2>
 
-            {/* Header */}
-            <h2 className=' text-white text-5xl tracking-tight font-bold text-center uppercase lg:mb-10 lg:text-7xl '>
-                See What People are saying<span className='text-yellow-500'>.</span>
-            </h2>
+      {/* Reviews */}
+      <div className='max-w-[1000px] relative mx-auto mt-16'>
+        <Swiper
+          ref={sliderRef}
+          slidesPerView={1}
+          loop={true}
+          className={`hidden  w-[80%] md:w-[75%] rounded-2xl sm:block `}>
+          {ReviewsContent.map((item, idx) => (
+            <SwiperSlide
+              key={`Review-${idx}`}
+              className='flex flex-col space-y-5 items-center w-[200px] bg-white p-5'>
+              <ImQuotesRight className='text-yellow-500 text-[3rem]' />
+              <p className='w-[90%] text-center'>{item.content}</p>
+              <span className='font-bold text-2xl uppercase'>
+                {item.author}
+              </span>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-            {/* Reviews */}
-            <div className='max-w-[1000px] relative mx-auto mt-16'>
-                <Swiper
-                    ref={sliderRef}
-                    slidesPerView={1}
-                    loop={true}
-                    className={`hidden  w-[80%] rounded-2xl sm:block `}
-                >
-                    {ReviewsContent.map((item, idx) => (
-                        <SwiperSlide key={`Review-${idx}`} className='flex flex-col space-y-5 items-center w-[200px] bg-white p-5'>
-                            <ImQuotesRight className="text-yellow-500 text-[3rem]" />
-                            <p className='w-[90%] text-center'>{item.content}</p>
-                            <span className='font-bold text-2xl uppercase'>{item.author}</span>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+        {/* Navigation Arrows */}
+        <span className='hidden sm:inline-block absolute top-[50%] -translate-y-[50%] left-10 cursor-pointer'>
+          <GoChevronLeft
+            className='   text-yellow-500 text-5xl'
+            onClick={handlePrev}
+          />
+        </span>
+        <span className='hidden sm:inline-block absolute top-[50%] -translate-y-[50%] right-10 cursor-pointer'>
+          <GoChevronRight
+            className=' text-yellow-500 text-5xl'
+            onClick={handleNext}
+          />
+        </span>
+      </div>
 
-                {/* Navigation Arrows */}
-                <span className='hidden sm:inline-block absolute top-[50%] -translate-y-[50%] left-10 cursor-pointer'>
-                    <GoChevronLeft className='   text-yellow-500 text-5xl' onClick={handlePrev} />
-                </span>
-                <span className='hidden sm:inline-block absolute top-[50%] -translate-y-[50%] right-10 cursor-pointer'>
-                    <GoChevronRight className=' text-yellow-500 text-5xl' onClick={handleNext} />
-                </span>
-            </div>
-
-            {/* Mobile Reviews */}
-            <Swiper
-                slidesPerView={1}
-                modules={[Pagination]}
-                pagination={{ clickable: true }}
-                loop={true}
-                className={` sm:hidden w-[80%]  bg-white rounded-2xl`}
-            >
-                {ReviewsContent.map((item, idx) => (
-                    <SwiperSlide key={`Review-${idx}`} className='mb-12  flex flex-col space-y-5 items-center w-[200px] bg-white p-5'>
-                        <ImQuotesRight className="text-yellow-500 text-[3rem]" />
-                        <p className='w-[90%] text-center'>{item.content}</p>
-                        <span className='font-bold text-2xl uppercase'>{item.author}</span>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-
-        </div>
-
-    )
+      {/* Mobile Reviews */}
+      <Swiper
+        slidesPerView={1}
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+        loop={true}
+        className={` sm:hidden w-[80%]  bg-white rounded-2xl`}>
+        {ReviewsContent.map((item, idx) => (
+          <SwiperSlide
+            key={`Review-${idx}`}
+            className='mb-12  flex flex-col space-y-5 items-center w-[200px] bg-white p-5'>
+            <ImQuotesRight className='text-yellow-500 text-[3rem]' />
+            <p className='w-[90%] text-center'>{item.content}</p>
+            <span className='font-bold text-2xl uppercase'>{item.author}</span>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
-export default Reviews
+export default Reviews;
