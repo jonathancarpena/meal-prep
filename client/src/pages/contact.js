c6604cccf6eecb373cb26b7ef7d53a8cd735269a
Jonathan Carpena <63978806+jonathancarpena@users.noreply.github.com>
2022-09-12 16:33:24 -0700

    Fixed contact page responsive

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,218 +1,228 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
-    validateEmail,
-    // sendEmail
-} from '../lib/utils'
+  validateEmail,
+  // sendEmail
+} from '../lib/utils';
 
 // Icons
-import { CgSpinnerTwoAlt } from 'react-icons/cg'
+import { CgSpinnerTwoAlt } from 'react-icons/cg';
 
-
-const FormInput = ({ error, value, setValue, label, type = "text" }) => {
-    function generateErrorMessage() {
-        if (label.toLowerCase() === "first name" || label.toLowerCase() === "last name") {
-            return "Min. 3 Characters."
-        } else if (label.toLowerCase() === "order number") {
-            return "Please Provide an Order Number."
-        } else if (label.toLowerCase() === "email") {
-            return "Invalid Email."
-        } else {
-            return "Please Provide an Input."
-        }
+const FormInput = ({ error, value, setValue, label, type = 'text' }) => {
+  function generateErrorMessage() {
+    if (
+      label.toLowerCase() === 'first name' ||
+      label.toLowerCase() === 'last name'
+    ) {
+      return 'Min. 3 Characters.';
+    } else if (label.toLowerCase() === 'order number') {
+      return 'Please Provide an Order Number.';
+    } else if (label.toLowerCase() === 'email') {
+      return 'Invalid Email.';
+    } else {
+      return 'Please Provide an Input.';
     }
-    return (
-        <div className='flex flex-col space-y-1 w-full'>
-            <label className={`text-neutral-700`} >
-                <span className='capitalize'>{label}</span>
-                {error &&
-                    <span className='font-semibold text-sm text-red-500 ml-3'>
-                        {generateErrorMessage()}
-                    </span>
-                }
-            </label>
-            {type === "text" &&
-                <input
-                    type="text"
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    className='border-2 border-neutral-500  p-3 rounded-lg focus:outline-2 focus:outline-offset-1 focus:outline-sky-500 text-neutral-900'
-                />
-            }
+  }
+  return (
+    <div className='flex flex-col space-y-1 w-full'>
+      <label className={`text-neutral-700`}>
+        <span className='capitalize'>{label}</span>
+        {error && (
+          <span className='font-semibold text-sm text-red-500 ml-3'>
+            {generateErrorMessage()}
+          </span>
+        )}
+      </label>
+      {type === 'text' && (
+        <input
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className='border-2 border-neutral-500  p-3 rounded-lg focus:outline-2 focus:outline-offset-1 focus:outline-sky-500 text-neutral-900'
+        />
+      )}
 
-            {type === "email" &&
-                <input
-                    type={"email"}
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    className='border-2 border-neutral-500  p-3 rounded-lg focus:outline-2 focus:outline-offset-1 focus:outline-sky-500 text-neutral-900'
-                />
-            }
+      {type === 'email' && (
+        <input
+          type={'email'}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className='border-2 border-neutral-500  p-3 rounded-lg focus:outline-2 focus:outline-offset-1 focus:outline-sky-500 text-neutral-900'
+        />
+      )}
 
-            {type === "textarea" &&
-                <textarea
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    className='border-2 border-neutral-500  p-3 rounded-lg focus:outline-2 focus:outline-offset-1 focus:outline-sky-500 text-neutral-900'
-                />
-            }
-
-        </div>
-    )
-}
+      {type === 'textarea' && (
+        <textarea
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className='border-2 border-neutral-500  p-3 rounded-lg focus:outline-2 focus:outline-offset-1 focus:outline-sky-500 text-neutral-900'
+        />
+      )}
+    </div>
+  );
+};
 
 function Contact() {
-    const [loading, setLoading] = useState(false)
-    const [fName, setFName] = useState('')
-    const [lName, setLName] = useState('')
-    const [email, setEmail] = useState('')
-    const [orderNum, setOrderNum] = useState('')
-    const [message, setMessage] = useState('')
-    const [errors, setErrors] = useState({ fName: null, lName: null, email: null, orderNum: null, message: null })
+  const [loading, setLoading] = useState(false);
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+  const [email, setEmail] = useState('');
+  const [orderNum, setOrderNum] = useState('');
+  const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({
+    fName: null,
+    lName: null,
+    email: null,
+    orderNum: null,
+    message: null,
+  });
 
-    function inputValidation(input, email) {
-        let error = false
-        if (input.length < 3) {
-            error = true
-        } else {
-            if (email) {
-                error = validateEmail(input)
-            }
-        }
-        return error
+  function inputValidation(input, email) {
+    let error = false;
+    if (input.length < 3) {
+      error = true;
+    } else {
+      if (email) {
+        error = validateEmail(input);
+      }
+    }
+    return error;
+  }
+
+  function formValidate() {
+    const copy = { ...errors };
+    if (inputValidation(fName, false)) {
+      copy['fName'] = true;
+    } else {
+      copy['fName'] = false;
     }
 
-    function formValidate() {
-        const copy = { ...errors }
-        if (inputValidation(fName, false)) {
-            copy['fName'] = true
-        } else {
-            copy['fName'] = false
-        }
-
-        if (inputValidation(lName, false)) {
-            copy['lName'] = true
-        } else {
-            copy['lName'] = false
-        }
-
-        if (inputValidation(email, true)) {
-            copy['email'] = true
-        } else {
-            copy['email'] = false
-        }
-
-        if (inputValidation(orderNum, false)) {
-            copy['orderNum'] = true
-        } else {
-            copy['orderNum'] = false
-        }
-
-        if (inputValidation(message, false)) {
-            copy['message'] = true
-        } else {
-            copy['message'] = false
-        }
-
-        setErrors({ ...copy })
-
-        const results = Object.values(copy).every((item) => item === false)
-        return results
+    if (inputValidation(lName, false)) {
+      copy['lName'] = true;
+    } else {
+      copy['lName'] = false;
     }
 
-    function handleFormReset() {
-        setFName('')
-        setLName('')
-        setEmail('')
-        setOrderNum('')
-        setMessage('')
-        setErrors({ fName: null, lName: null, email: null, orderNum: null, message: null })
+    if (inputValidation(email, true)) {
+      copy['email'] = true;
+    } else {
+      copy['email'] = false;
     }
-    function handleContactSubmit(e) {
-        e.preventDefault()
-        if (formValidate()) {
-            // Send Email
-            // const params = {
-            //     first_name: fName,
-            //     last_name: lName,
-            //     email: email,
-            //     order_num: orderNum,
-            //     message: message,
-            // }
-            // sendEmail(params)
 
-            setLoading(true)
-            setTimeout(() => {
-                handleFormReset()
-                setLoading(false)
-                alert("Thank you your message was sent! We'll get back to you in 24-48 hours.")
-            }, [2000])
-        }
+    if (inputValidation(orderNum, false)) {
+      copy['orderNum'] = true;
+    } else {
+      copy['orderNum'] = false;
     }
-    return (
-        <div className='mt-[90px]  min-h-[91vh] p-5 lg:p-20 bg-neutral-100 flex justify-center items-center'>
-            <div className="flex flex-col space-y-5 bg-white rounded-xl p-5 drop-shadow-xl max-w-[700px]">
-                <h1 className='font-semibold text-4xl'>
-                    Contact Us
-                </h1>
-                <p>
-                    Any questions about the products, your order,
-                    inquiries etc...Shoot us an email at:
-                    Support@FinalBossPerformance.com
-                </p>
 
-                <p>If you have any questions about shipping &
-                    returns please see our Shipping and Returns
-                    tab at the bottom of the home page.
-                </p>
+    if (inputValidation(message, false)) {
+      copy['message'] = true;
+    } else {
+      copy['message'] = false;
+    }
 
-                <form onSubmit={handleContactSubmit} className='flex flex-col space-y-5'>
+    setErrors({ ...copy });
 
-                    <div className='flex flex-col space-y-5 sm:flex-row sm:space-y-0 sm:space-x-5'>
-                        <FormInput
-                            error={errors.fName}
-                            value={fName}
-                            setValue={setFName}
-                            label={"First Name"}
-                        />
-                        <FormInput
-                            error={errors.lName}
-                            value={lName}
-                            setValue={setLName}
-                            label={"Last Name"}
-                        />
-                    </div>
+    const results = Object.values(copy).every((item) => item === false);
+    return results;
+  }
 
-                    <FormInput
-                        error={errors.email}
-                        type='email'
-                        value={email}
-                        setValue={setEmail}
-                        label={'Email'}
-                    />
+  function handleFormReset() {
+    setFName('');
+    setLName('');
+    setEmail('');
+    setOrderNum('');
+    setMessage('');
+    setErrors({
+      fName: null,
+      lName: null,
+      email: null,
+      orderNum: null,
+      message: null,
+    });
+  }
+  function handleContactSubmit(e) {
+    e.preventDefault();
+    if (formValidate()) {
+      // Send Email
+      // const params = {
+      //     first_name: fName,
+      //     last_name: lName,
+      //     email: email,
+      //     order_num: orderNum,
+      //     message: message,
+      // }
+      // sendEmail(params)
 
-                    <FormInput
-                        error={errors.orderNum}
-                        value={orderNum}
-                        setValue={setOrderNum}
-                        label={'Order Number'}
-                    />
+      setLoading(true);
+      setTimeout(() => {
+        handleFormReset();
+        setLoading(false);
+        alert(
+          "Thank you your message was sent! We'll get back to you in 24-48 hours."
+        );
+      }, [2000]);
+    }
+  }
+  return (
+    <div className=' mt-[45px] md:mt-[10px] lg:mt-0  min-h-[91vh] p-5 lg:p-20 bg-neutral-100 flex justify-center items-center'>
+      <div className='flex flex-col space-y-5 bg-white rounded-xl p-5 drop-shadow-xl max-w-[700px]'>
+        <h1 className='font-semibold text-4xl'>Contact Us</h1>
+        <p>
+          Any questions about the products, your order, inquiries etc...Shoot us
+          an email at: Support@FinalBossPerformance.com
+        </p>
 
-                    <FormInput
-                        error={errors.message}
-                        value={message}
-                        setValue={setMessage}
-                        label={'Message'}
-                        type={"textarea"}
-                    />
+        <form
+          onSubmit={handleContactSubmit}
+          className='flex flex-col space-y-5'>
+          <div className='flex flex-col space-y-5 sm:flex-row sm:space-y-0 sm:space-x-5'>
+            <FormInput
+              error={errors.fName}
+              value={fName}
+              setValue={setFName}
+              label={'First Name'}
+            />
+            <FormInput
+              error={errors.lName}
+              value={lName}
+              setValue={setLName}
+              label={'Last Name'}
+            />
+          </div>
 
-                    <button className='hover:bg-yellow-400 bg-yellow-500 rounded-lg text-white text-xl font-semibold py-3 px-5 w-max self-end'>
-                        {loading && <CgSpinnerTwoAlt className='inline-block mr-2 animate-spin mb-1' />}Submit
-                    </button>
-                </form>
-            </div>
+          <FormInput
+            error={errors.email}
+            type='email'
+            value={email}
+            setValue={setEmail}
+            label={'Email'}
+          />
 
-        </div>
-    )
+          <FormInput
+            error={errors.orderNum}
+            value={orderNum}
+            setValue={setOrderNum}
+            label={'Order Number'}
+          />
+
+          <FormInput
+            error={errors.message}
+            value={message}
+            setValue={setMessage}
+            label={'Message'}
+            type={'textarea'}
+          />
+
+          <button className='hover:bg-yellow-400 bg-yellow-500 rounded-lg text-white text-xl font-semibold py-3 px-5 w-max self-end'>
+            {loading && (
+              <CgSpinnerTwoAlt className='inline-block mr-2 animate-spin mb-1' />
+            )}
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
